3b88bdaf130864e68520d996b9e08df00490fbb6
tarequr <tarequrr8@gmail.com>
2024-08-13 21:09:05 +0600

    type define

--- 01.number/output/script.js
+++ 01.number/output/script.js
@@ -10,3 +10,11 @@ let person = {
 };
 // person.company = "Nextgen";
 console.log(person);
+/***************************** TYPE DEFINE *****************************/
+let a = "Tayebur Rahman Tipu";
+let b = 500;
+let c = false;
+let d = ["Jannt", "Radin", "Rafin", "Rifat", "Raisan"];
+let e = [10, 20, 30, 40, 50, 60, 70, 80, 100];
+let f = { "a": 1, "b": 2, "c": 3 };
+console.log(a, b, c, d, e, f);
--- 01.number/src/script.ts
+++ 01.number/src/script.ts
@@ -14,3 +14,14 @@ let person = {
 // person.company = "Nextgen";
 
 console.log(person);
+
+
+/***************************** TYPE DEFINE *****************************/
+let a : string   = "Tayebur Rahman Tipu";
+let b : number   = 500;
+let c : boolean  = false;
+let d : string[] = ["Jannt", "Radin", "Rafin", "Rifat", "Raisan"];
+let e : number[] = [10, 20, 30, 40, 50, 60, 70, 80, 100];
+let f : object = {"a": 1, "b": 2, "c": 3};
+
+console.log(a, b, c, d, e, f);
