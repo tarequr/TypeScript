d7d4fa7a57d61811a76ff531499912168f71a924
tarequr <tarequrr8@gmail.com>
2024-08-13 20:59:45 +0600

    created an object

--- 01.number/output/script.js
+++ 01.number/output/script.js
@@ -2,3 +2,11 @@
 console.log("Hello TypeScript");
 const country = "Bangladesh";
 console.log(country);
+let person = {
+    "name": "Tarequr Rahman Sabbir",
+    "age": 28,
+    "country": "Bangladesh",
+    "isMuslims": true
+};
+// person.company = "Nextgen";
+console.log(person);
