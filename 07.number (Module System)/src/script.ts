eb54b9abc0feb084a09bf9ef0a7d0c37ed085876
tarequr <tarequrr8@gmail.com>
2024-08-18 20:49:33 +0600

    Module System

---
+++ 07.number (Module System)/src/script.ts
@@ -0,0 +1,16 @@
+import { Player } from "./classes/Player.js";
+
+const tamim = new Player('Tamim', 38, 'Bangladesh');
+const miraz = new Player('Miraz', 28, 'Bangladesh');
+
+console.log(tamim.age);
+console.log(tamim.country);
+
+const players: Player[] = [];
+
+players.push(tamim);
+players.push(miraz);
+
+// NOTE
+    // ->Change the "module": "ES2015",    tsconfig.json
+    // ->Change the type="module" <script type="module" src="./output/script.js"></script>
