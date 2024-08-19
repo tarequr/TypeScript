339753b25755d13c8c9bf7813d1f9d2fdb9ac078
tarequr <tarequrr8@gmail.com>
2024-08-19 21:23:29 +0600

    Tuples

---
+++ 11.number (Tuples)/src/script.ts
@@ -0,0 +1,5 @@
+//Tuples 
+let a = ["Tanzim Hasan Radin", 15, { father: "Mozammel", mother: "Ruma" }];
+
+
+let b:[string, number, object] = ["Tahmid Hasan Rafi", 7, { father: "Mozammel", mother: "Ruma" }];  //Here we define the array type. It's called Tuples
